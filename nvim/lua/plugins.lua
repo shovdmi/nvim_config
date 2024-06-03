b6c9d9af942904216fbbc9199f8200d6d40604b2
Дмитрий Шовкун <d.shovkun@atol.ru>
2024-06-03 10:48:55 +0300

    Add basic Lspconfig
    Don't forget to clone :
    git clone https://github.com/neovim/nvim-lspconfig C:/Work/nvim_config_test/nvim/pack/nvim/start/nvim-lspconfig

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,2 +1,19 @@
 require('minintro').setup{
 }
+
+
+require'lspconfig'.clangd.setup{
+-- https://stackoverflow.com/questions/76625619/is-it-possible-to-override-or-augment-neovims-lsp-configuration-via-a-workspace
+
+   --cmd = {
+   --     "~/.espressif/tools/esp-clang/bin/clangd",
+   --     "--pretty",
+   --     "--header-insertion=iwyu",
+   --     .....
+   --     "--compile-commands-dir=.",
+   -- },
+    filetypes = { "c", "cc", "cpp", "objc", "objcpp", "cuda", "proto" },
+    function(fname)
+      return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname)
+    end,
+}
