76bd70e893247766c86680b429eccfb2263dcf02
Дмитрий Шовкун <d.shovkun@atol.ru>
2024-06-03 10:47:08 +0300

    Add startup welcome screen plugin

---
+++ nvim/lua/minintro/welcome_text.lua
@@ -0,0 +1,12 @@
+local intro_logo = {
+	" This intro is in:                                      ",
+		"~\\AppData\\Local\\nvim\\lua\\minintro\\init.lua",
+		--script_path(),
+	"                                                       ",
+	":Expore or :Vexplore for netrw                         ",
+	":e ~\\AppData\\Local\\nvim\\init.lua                       ",
+	"                                                       "
+}
+
+return intro_logo
+
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,2 @@
+require('minintro').setup{
+}
