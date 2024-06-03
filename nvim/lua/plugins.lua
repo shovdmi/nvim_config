c5d60326fab12889711107fda8f68fae65388f00
Дмитрий Шовкун <d.shovkun@atol.ru>
2024-06-03 14:43:18 +0300

    Add nvim-cmp plugin config
    ```
    git clone https://github.com/hrsh7th/nvim-cmp.git C:/Work/nvim_config_test/nvim/pack/nvim/start/nvim-cmp
    git clone https://github.com/onsails/lspkind.nvim C:/Work/nvim_config_test/nvim/pack/nvim/start/lspkind.nvim
    git clone https://github.com/hrsh7th/cmp-nvim-lsp.git C:/Work/nvim_config_test/nvim/pack/nvim/start/cmp-nvim-lsp
    git clone https://github.com/L3MON4D3/LuaSnip.git C:/Work/nvim_config_test/nvim/pack/nvim/start/LuaSnip
    ```

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,6 +2,75 @@ require('minintro').setup{
 }
 
 
+-- Setup nvim-cmp.
+local cmp = require("cmp")
+local lspkind = require("lspkind")
+
+
+-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+cmp.setup{
+	snippet = {
+
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			require'luasnip'.lsp_expand(args.body) -- Luasnip expand
+		end,
+	},
+
+	-- Клавиши, которые будут взаимодействовать в nvim-cmp
+	mapping = cmp.mapping.preset.insert {
+          ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end,
+          ['<S-Tab>'] = function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end,
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Esc>'] = cmp.mapping.close(),
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+		-- Вызов меню автодополнения
+		['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+		--['<CR>'] = cmp.config.disable,                      -- Я не люблю, когда вещи автодополняются на <Enter>
+		['<C-y>'] = cmp.mapping.confirm({ select = true }), -- А вот на <C-y> вполне ок
+
+		-- Используем <C-e> для того чтобы прервать автодополнение
+		['<C-e>'] = cmp.mapping({
+			i = cmp.mapping.abort(), -- Прерываем автодополнение
+			c = cmp.mapping.close(), -- Закрываем автодополнение
+		}),
+		['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+		['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+	},
+
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		--{ name = 'nvim_lsp_signature_help' }, -- Помощь при введении параметров в методах 🚁
+		{ name = 'buffer' },
+		{ name = 'path' },
+		{ name = "emoji" },
+	}, {
+	}),
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = 'symbol', -- show only symbol annotations
+			maxwidth = 50,   -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+		})
+	}
+}
+
+
 require'lspconfig'.clangd.setup{
 -- https://stackoverflow.com/questions/76625619/is-it-possible-to-override-or-augment-neovims-lsp-configuration-via-a-workspace
 
@@ -13,6 +82,7 @@ require'lspconfig'.clangd.setup{
    --     "--compile-commands-dir=.",
    -- },
     filetypes = { "c", "cc", "cpp", "objc", "objcpp", "cuda", "proto" },
+    capabilities = lsp_capabilities,
     function(fname)
       return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname)
     end,
